35ee9476e5ddc5137df03110025ff7e1657a09a0
Nilton Moura <moura.nilton@gmail.com>
2014-06-03 17:45:31 -0300

    Changed group cmd to individual cmd
    Changed the stop-group and start-group to stop and start commands, because the group commands doesn't have the blocking attribute, which can be used to ensure if the stop or start commands was successfully or not.

--- jb6_artifact_deploy.sh
+++ jb6_artifact_deploy.sh
@@ -17,9 +17,6 @@
 # ----------------------------------------------------------------------------
 #
 
-#
-# Beggining
-#
 usage_message="
 Usage: $(basename "$0") -a file -g group [-c configuration file] \\
 {-e environment | -d domain -u username -s password} [-p port]
@@ -104,123 +101,175 @@ done
 # Parser for the configuration file.
 #
 parse_conf(){
-    echo $(sed -e '/./{H;$!d;}' -e 'x;/\['$1'\]/b' -e d "$conffile" \
+  echo $(sed -e '/./{H;$!d;}' -e 'x;/\['$1'\]/b' -e d "$conffile" \
 | grep ^"$2 "= | cut -d '=' -f 2)
 }
 
 #
-# Check if the parameters were correctly passed
+# Check if the parameters were correctly passed.
 #
 if test -z "$conffile" ; then
-    conffile="$(dirname $0)/jb6_artifact_deploy.conf"
+  conffile="$(dirname $0)/jb6_artifact_deploy.conf"
 fi
 
 if test -z "$environment" ; then
-    if [ -z "$domain" ] || [ -z "$username" ] || [ -z "$password" ] ; then
-        echo -n "ERROR: inform the environment or domain/username/password. "
-        echo "Run \"$0 -h\" to help."
-        exit 1
-    fi
+  if [ -z "$domain" ] || [ -z "$username" ] || [ -z "$password" ] ; then
+    echo -n "ERROR: inform the environment or domain/username/password. "
+    echo "Run \"$0 -h\" to help."
+    exit 1
+  fi
 fi
 
 if [ -z "$group" ] || [ -z "$artifact" ] ; then
-    echo -n "ERROR: artifact and group must be informed. "
-    echo "Run \"$0 -h\" to help."
-    exit 1
+  echo -n "ERROR: artifact and group must be informed. "
+  echo "Run \"$0 -h\" to help."
+  exit 1
 fi
 
 if test -z "$port" ; then
-    port=9999
+  port=9999
 fi
 
 #
-# Get environment data from the configuration file
+# Get environment data from the configuration file.
 #
 if test -n "$environment" ; then
-    if test -z $domain ; then
-        domain=$(parse_conf $environment domain)
-    fi
-    if test -z $port ; then
-        port=$(parse_conf $environment port)
-    fi
-    if test -z $username ; then
-        username=$(parse_conf $environment username)
-    fi
-    if test -z $password ; then
-        password=$(parse_conf $environment password)
-    fi
+  if test -z $domain ; then
+    domain=$(parse_conf $environment domain)
+  fi
+  if test -z $port ; then
+    port=$(parse_conf $environment port)
+  fi
+  if test -z $username ; then
+    username=$(parse_conf $environment username)
+  fi
+  if test -z $password ; then
+    password=$(parse_conf $environment password)
+  fi
 fi
 
 if [ -z "$domain" ] || [ -z "$group" ] || [ -z "$username" ] \
 || [ -z "$password" ] ; then
-    echo -n "ERROR: put the domain/username/password in the $environment "
-    echo -n "section, in the $(basename $conffile) configuration file. "
-    echo "Run \"$0 -h\" to help."
-    exit 1
+  echo -n "ERROR: put the domain/username/password in the $environment "
+  echo -n "section, in the $(basename $conffile) configuration file. "
+  echo "Run \"$0 -h\" to help."
+  exit 1
 fi
 
 jboss_dir=$(parse_conf global jboss_dir)
 
 if [ ! -d $jboss_dir ] || [ ! -x $jboss_dir/bin/jboss-cli.sh ]; then
-    echo -n "ERROR: put the JBoss directory in the jboss_dir variable, in "
-    echo "the [global] section in the $conffile."
-    exit 1
+  echo -n "ERROR: put the JBoss directory in the jboss_dir variable, in "
+  echo "the [global] section in the $conffile."
+  exit 1
 fi
 
 #
-# Deploy
+# This section creates a variable called server_in_hc which is a list of
+# host_controller/server_instance entries, to treat them individually,
+# because stop-group and start-group commands doesn't have blocking attribute.
+# So, there's not a way to assure success or not, unless using stop and start
+# commands for each server instance individually.
 #
-# 1. Stop servers;
-# 2. Undeploy artifact;
-# 3. Deploy artifact;
-# 4. Start servers.
-#
+hcs=$($jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+--user=$username --password=$password --command="ls /host=")
 
+for hc in $hcs ; do
+
+  hc_srvconf=$($jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+  --user=$username --password=$password --command="ls /host=$hc/server-config")
+
+  for server in $hc_srvconf ; do
+    hc_srvgroup=$($jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+    --user=$username --password=$password \
+    --command="/host=$hc/server-config=$server:read-attribute(name=group)" \
+    | grep $group)
+
+    if [ $? == "0" ] ; then
+      server_in_hc="$server_in_hc $hc/$server"
+    fi
+
+  done
+
+done
+
+#
+# Stop each server individually.
+#
+for entry in $server_in_hc ; do
+
+  hc=$(echo $entry | cut -d '/' -f 1)
+  server=$(echo $entry | cut -d '/' -f 2)
+
+  echo "Stopping $server on $hc."
+  $jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+  --user=$username --password=$password \
+  --command="/host=$hc/server-config=$server:stop(blocking=true)" \
+  &> /dev/null
+
+  if [ $? != "0" ] ; then
+    echo "ERROR: Unsuccessful stop command for $server from $hc."
+    exit 1
+  fi
+
+done
+
+#
+# Check if the server group has the $filename deployed. If true, do the
+# undeploy on the server group.
+#
 filename=$(basename $artifact)
 
-$jboss_dir/bin/jboss-cli.sh --connect --controller=$domain --user=$username \
---password=$password --command="/server-group=$group:stop-servers()"
+$jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+--user=$username --password=$password \
+--command="ls /server-group=$group/deployment" | grep ^"$filename"$ \
+&> /dev/null
 
 if test $? == '0' ; then
+
+    echo "Undeploying $filename from $group server group."
     $jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
     --user=$username --password=$password \
-    --command="ls /server-group=$group/deployment" | grep ^"$filename"$ \
+    --command="undeploy $filename --server-groups=$group" \
     &> /dev/null
-else
-    # This doesn't ensure that the process stopped successfully
-    # because, even if a problem occur, the exit status can be 0,
-    # with just an '"result" => undefined' message, that we
-    # can't use, because this is the same message if the stopped
-    # was successfully.
-    echo "ERROR: problem to stop the group server $group."
-    exit 1
+
+    if test $? != '0' ; then
+      echo "ERROR: problem to undeploy $filename on $group server group."
+      exit 1
+    fi
+
 fi
 
-if test $? == '0' ; then
-    $jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
-    --user=$username --password=$password \
-    --command="undeploy $filename --server-groups=$group"
-fi
-
-if test $? == '0' ; then
-    $jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
-    --user=$username --password=$password \
-    --command="deploy $artifact --server-groups=$group"
-else
-    echo "ERROR: problem to undeploy $filename."
-    exit 1
-fi
-
-if test $? == '0' ; then
-    $jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
-    --user=$username --password=$password \
-    --command="/server-group=$group:start-servers()"
-else
-    echo "ERROR: problem to deploy $filename."
-    exit 1
-fi
+#
+# Deploy the $filename on the server group.
+#
+echo "Deploying $filename on $group server group."
+$jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+--user=$username --password=$password \
+--command="deploy $artifact --server-groups=$group"
 
 if test $? != '0' ; then
-    echo "ERROR: problem to start server group $group."
-    exit 1
+  echo "ERROR: problem to deploy $filename on $group server group."
+  exit 1
 fi
+
+#
+# Start each server individually.
+#
+for entry in $server_in_hc ; do
+
+  hc=$(echo $entry | cut -d '/' -f 1)
+  server=$(echo $entry | cut -d '/' -f 2)
+
+  echo "Starting $server on $hc."
+  $jboss_dir/bin/jboss-cli.sh --connect --controller=$domain \
+  --user=$username --password=$password \
+  --command="/host=$hc/server-config=$server:start(blocking=true)" \
+  &> /dev/null
+
+  if [ $? != "0" ] ; then
+    echo "ERROR: Unsuccessful start command for $server on $hc."
+    exit 1
+  fi
+
+done
